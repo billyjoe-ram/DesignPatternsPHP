f8b0ac6602c9080e6904bcf519ade7ccfd581ab7
Billy Joe <billyjoersantos@outlook.com>
2022-06-20 20:58:51 -0300

    feature(chain-of-responsibility): Design Patterns - Chain of Responsibility
    Aprimorando a classe de orçamento para incluir itens no orçamento.
    Esse recurso será utilizado para definir novas regras por meio do
    Chain of Responsibility

--- src/Orcamento.php
+++ src/Orcamento.php
@@ -5,4 +5,15 @@ namespace Alura\DesignPattern;
 class Orcamento
 {
     public float $valor;
+    private array $itens = [];
+
+    public function setItens(array $itens)
+    {
+        $this->itens = $itens;
+    }
+
+    public function getItens() : array
+    {
+        return $this->itens;
+    }
 }
