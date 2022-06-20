e417e425bba7c5863bdcb4682bbda122af4f690d
Billy Joe <billyjoersantos@outlook.com>
2022-06-20 20:59:47 -0300

    feature(chain-of-responsibility): Design Patterns - Chain of Responsibility
    Adicionando testes para o programa

--- testes.php
+++ testes.php
@@ -1,14 +1,32 @@
 <?php
 
+use Alura\DesignPattern\CalculadoraDescontos;
 use Alura\DesignPattern\Orcamento;
-use Alura\DesignPattern\CalculadoraImpostos;
-use Alura\DesignPattern\Impostos\Icms;
+// use Alura\DesignPattern\CalculadoraImpostos;
+// use Alura\DesignPattern\Impostos\Icms;
 
 require 'vendor/autoload.php';
 
-$calculadora = new CalculadoraImpostos();
+// $calculadora = new CalculadoraImpostos();
+
+// $orcamento = new Orcamento();
+// $orcamento->valor = 100;
+
+// echo $calculadora->calcula($orcamento, new Icms());
+
+$calculadora = new CalculadoraDescontos();
 
 $orcamento = new Orcamento();
-$orcamento->valor = 100;
 
-echo $calculadora->calcula($orcamento, new Icms());
+$itensOrcamento = [];
+
+for ($i=0; $i < 10; $i++) { 
+    $itensOrcamento[] = $i;
+}
+$orcamento->setItens($itensOrcamento);
+
+$orcamento->valor = 600;
+
+echo $calculadora->calcula($orcamento);
+
+
